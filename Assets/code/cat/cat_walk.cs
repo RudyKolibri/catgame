73496747a9dafecf307237c4a530ce4c5f379785
RudyKolibri <vanammelr@gmail.com>
2024-11-05 19:54:46 +0100

    line of sight added
    i added line of sight when they see eachather the go to there first positiion

--- Assets/code/cat/cat_walk.cs
+++ Assets/code/cat/cat_walk.cs
@@ -1,37 +1,47 @@
 using LDtkUnity;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class cat_walk : MonoBehaviour
 {
     LDtkFields data;
     int index = 0;
+    public bool walking = false;
+    public LDtkReferenceToAnEntityInstance[] test;
     public Vector2[] path;
     Transform cat;
     private void Start()
     {
         data = GetComponent<LDtkFields>();
         path = data.GetPointArray("path");
+        test = data.GetEntityReferenceArray("othercats");
         start_movement();
         transform.position = path[0];
     }
     public void start_movement() // will be called by a play button
     {
+        walking= true;
         Invoke("step", 0.5f);
     }
     public void step()
     {
-        index += 1;
-        transform.position = path[index];
-        
-        if (index == path.Length - 1) {
-            Invoke("finish", 0.5f);
-        }
-        else
+        if (walking != false)
         {
-            Invoke("step", 0.5f);
+            index += 1;
+            transform.position = path[index];
+
+            if (index == path.Length - 1)
+            {
+                Invoke("finish", 0.5f);
+            }
+            else
+            {
+                Invoke("step", 1f);
+            }
         }
+
     }
     public void finish()
     {
@@ -39,16 +49,28 @@ public class cat_walk : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, cat.position - transform.position);
-        if (ray.collider != null)
-        {
-            //hit
-            Invoke("fail",0.1f);
+        foreach (LDtkReferenceToAnEntityInstance i in test) {
+            GetComponent<CircleCollider2D>().enabled = false;
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, i.FindEntity().transform.position- transform.position);
+            Debug.DrawRay(transform.position, i.FindEntity().transform.position - transform.position);
+            if (ray.collider != null)
+            {
+                
+                
+                if (ray.collider.tag == "cat")
+                {
+                    Debug.Log(ray.collider);
+                    walking = false;
+                    Invoke("fail", 0.1f);
+                }
+            }
+            GetComponent<CircleCollider2D>().enabled = true;
         }
     }
     private void fail()
     {
         transform.position = path[0];
+        walking = false;
         index = 0;
     }
 }
