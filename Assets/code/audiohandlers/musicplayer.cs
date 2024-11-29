3cbe7beda888ac8dea4f7a0306edf00f8e02f9a6
RudyKolibri <vanammelr@gmail.com>
2024-11-29 17:34:37 +0100

    last push

--- Assets/code/audiohandlers/musicplayer.cs
+++ Assets/code/audiohandlers/musicplayer.cs
@@ -9,6 +9,7 @@ public class musicplayer : MonoBehaviour
     public AudioSource loopmainmenu;
     public AudioSource introgameplay;
     public AudioSource loopgameplay;
+    public AudioSource credits;
     
     public float offset;
     bool gameplay_looping = false;
@@ -51,12 +52,45 @@ public class musicplayer : MonoBehaviour
 
         Invoke("startgameplay",0.2f);
     }
+    public void changetocredits()
+    {
+
+        Invoke("startcredits",0.2f);
+    }
+    public void changetointro()
+    {
+
+        Invoke("startintro",0.2f);
+    }
     private void startgameplay()
     {
         loopmainmenu.Pause();
         intromainmenu.Pause();
         introgameplay.Play();
+        credits.Pause();
         gameplay_looping = false ;
         intro_looping=false ;
+    }
+    private void startcredits()
+    {
+        loopgameplay.Pause();
+        introgameplay.Pause();
+        intromainmenu.Pause();
+        intromainmenu.time = 0;
+        credits.time = 0;
+        credits.Play();
+        gameplay_looping = false ;
+        intro_looping =false ;
+    }
+    private void startintro()
+    {
+        loopgameplay.Pause();
+        introgameplay.Pause();
+        credits.Pause();
+        intromainmenu.Play();
+        intromainmenu.time = 0;
+        
+        gameplay_looping = false ;
+        intro_looping =false ;
     }
 }
--- Assets/code/gamelogic/scenemanager.cs
+++ Assets/code/gamelogic/scenemanager.cs
@@ -5,12 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class scenemanager : MonoBehaviour
 {
+    musicplayer music;
     public void play()
     {
-        Invoke("scenedelay", 0.05f);
+        Invoke("scenedelay_gameplay", 0.05f);
     }
-    private void scenedelay()
+    private void scenedelay_gameplay()
     {
         SceneManager.LoadScene("gameplay");
+    }
+    public void credits()
+    {
+        Invoke("scenedelay_credits", 0.05f);
+    }
+    private void scenedelay_credits()
+    {
+        SceneManager.LoadScene("credits");
+    }
+    public void mainmenu()
+    {
+        Invoke("scenedelay_mainmenu", 0.05f);
+    }
+    private void scenedelay_mainmenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+        music = FindObjectOfType<musicplayer>();
+        music.changetointro();
     }
 }
